d438747e7ec2448a8622ea6741bc146239c7c323
Jun922 <junjunchi620@gmail.com>
2024-03-30 23:29:46 +0900

    search func fin

--- pw_recorder/views.py
+++ pw_recorder/views.py
@@ -5,3 +5,12 @@ from .models import App
 class AppListView(generic.ListView):
     template_name = 'app/list.html'
     model = App
+
+    def get_queryset(self):
+        query = self.request.GET.get('query')
+
+        if query:
+            app_list = App.objects.filter(name__icontains=query)
+        else:
+            app_list = App.objects.all()
+        return app_list
