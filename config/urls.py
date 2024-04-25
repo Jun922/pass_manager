389324e86b6614204218b2ed25aa7b9b19a31c45
Jun922 <junjunchi620@gmail.com>
2024-04-26 00:07:14 +0900

    accounts signup

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("pw_recorder.urls")),
-    path('account/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
